a1fc9946364997373f3f60c13505ae3f590fb630
github matsutakechan2go <matsutakechan_nigo@aol.com>
2023-12-16 19:32:47 +0900

    条件分岐をcaseからwhileとifをつかったものに変更

--- password_manager.sh
+++ password_manager.sh
@@ -1,28 +1,38 @@
 #!/bin/bash
 
-echo  "パスワードマネージャーへようこそ！"
-echo  "次の選択肢から入力してください(Add Password/Get Password/Exit)："
-read  choice
+while true 
+do
+  echo  "パスワードマネージャーへようこそ！"
+  echo  "次の選択肢から入力してください(Add Password/Get Password/Exit)："
+  read  choice
 
-case $choice in
-"Add Password" )  
-  echo  "サービス名を入力してください:"
-  read  service 
-  echo "$service" >> service.txt
-  echo  "ユーザー名を入力してください:"
-  read  user 
-  echo "$user" >> user.txt
-  echo  "パスワードを入力してください:"
-  read  pass 
-  echo "$pass">> pass.txt 
-  echo "$service:$user:$pass"  >> index.txt
-  ;;
-"Get Password" )
-  echo  "サービス名を入力してください:"
-  ;;
-"Exit" )
+  if [ "$choice" = "Add Password" ]; then
+    echo  "サービス名を入力してください:"
+    read  service 
+    echo "$service" >> service.txt
+    echo  "ユーザー名を入力してください:"
+    read  user 
+    echo "$user" >> user.txt
+    echo  "パスワードを入力してください:"
+    read  pass 
+    echo "$pass" >> pass.txt 
+    echo "$service:$user:$pass" >> index.txt
+
+    elif [ "$choice" = "Get Password" ]; then
+      echo  "サービス名を入力してください:"
+      read Getsvc
+      grep "$Getsvc" 
+
+      if [ #サービス名が登録されてた場合 ]; then
+        echo "サービス名: \nユーザー名: \nパスワード:"
+      else [ #されてなかった場合 ]; then
+  
+  if ["$choice" = "Exit" ]
   echo  "Thank you!"
+  break
   ;;
  * )
   echo  "入力が間違えています。Add Password/Get Password/Exit から入力してください。" ;;
-  esac
+  fi
+
+  done
